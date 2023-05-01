9ce3957845b7ad60d89b23aec29fd65c4734fa97
Ignas0315 <ignaszakaitis@gmail.com>
2023-05-01 22:07:55 +0300

    JS-0014: Typo fix

--- Javascript-tests/Task 9/script.js
+++ Javascript-tests/Task 9/script.js
@@ -29,6 +29,7 @@ class Movie {
 
 const theShining = new Movie('The Shining', 'Stanley Kubrick', 19000000);
 console.log(`Movie ${theShining.title} was directed by ${theShining.director}`)
+
 if(theShining.wasExpensive()){
     console.log(`${theShining.title} had the budget of ${theShining.budget} $ and it is considered to be expensive`)
 } else {
