621d9be879190de819d789da5dd2daf87f19c14e
Ignas0315 <ignaszakaitis@gmail.com>
2023-05-02 17:26:20 +0300

    JS-0019: Exercise 2 formatting adjustment

--- Javascript-tests/Task 2/script.js
+++ Javascript-tests/Task 2/script.js
@@ -7,6 +7,8 @@ pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 let counterValue = parseInt(document.getElementById("btn__state").textContent);
 
 document.getElementById('btn__element').addEventListener('click', () => {
+
     counterValue++;
     document.getElementById('btn__state').innerHTML = counterValue;
+    
 })
